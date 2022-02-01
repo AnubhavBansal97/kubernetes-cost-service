b5e042b87194a64c12bdfa5babb8d185cf35a79c
anubhav.bansal@nutanix <anubhav.bansal@nutanix.com>
2022-02-01 12:34:00 +0530

    Initial commit

---
+++ src/main/java/com/hackathon/kubernetescostservice/KubernetesCostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.kubernetescostservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesCostServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesCostServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackathon/kubernetescostservice/KubernetesCostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.kubernetescostservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesCostServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
